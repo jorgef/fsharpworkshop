aa9f1023ad29b7effca7b5ae4a06ad69439e619a
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2014-09-22 13:39:48 +1000

    Fixed nre when selecting 0 customers

--- Module5/2.Post/Application/Services.fs
+++ Module5/2.Post/Application/Services.fs
@@ -7,11 +7,14 @@ type CustomerService() =
     member this.GetCustomers () = Data.getCustomers ()
 
     member this.UpgradeCustomers (ids: int seq) =
-        let customers = Data.getCustomers ()
-        ids 
-        |> Seq.map (fun id -> customers 
-                               |> Seq.find (fun c -> c.Id = id))
-        |> Seq.map (Functions.upgradeCustomer >> Data.updateCustomer)
-        |> List.ofSeq
-        |> ignore
+        match ids with
+        | null -> ()
+        | _ ->
+            let customers = Data.getCustomers ()
+            ids 
+            |> Seq.map (fun id -> customers 
+                                   |> Seq.find (fun c -> c.Id = id))
+            |> Seq.map (Functions.upgradeCustomer >> Data.updateCustomer)
+            |> List.ofSeq
+            |> ignore
 
