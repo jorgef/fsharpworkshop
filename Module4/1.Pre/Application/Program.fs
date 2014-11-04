47c87b31187e4e8d11bb485e7cc75652d0b4bb80
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2014-11-05 07:03:28 +1100

    Added required files to Module4 Pre

---
+++ Module4/1.Pre/Application/Program.fs
@@ -0,0 +1,15 @@
+﻿//module Program
+//
+//open System
+//open Services
+//open Types
+//
+//[<EntryPoint>]
+//let rec main args =
+//    let service = CustomerService ()
+//    service.GetCustomers () 
+//    |> Seq.iter (fun c -> printfn "Id: %A, IsVip: %b, Credit: %A" c.Id c.IsVip c.Credit)
+//    printf "Id to upgrade: "
+//    let id = int (Console.ReadLine ())
+//    service.UpgradeCustomer id
+//    main args
