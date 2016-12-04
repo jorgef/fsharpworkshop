7d0064011a35db955f8b79d9a639e9937ef5aa95
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2016-12-04 06:47:59 -0500

    Added validation for module4 app and fixed typos

--- Module4/Application/Program.fs
+++ Module4/Application/Program.fs
@@ -8,18 +8,22 @@ open Functions
 let rec main args =
 //    let service = CustomerService()
 //    printf "Id to upgrade [1-4]: "
-//    let id = int (Console.ReadLine ())
+//    let valid, id = Int32.TryParse <| Console.ReadLine ()
 //    printfn ""
-//    printfn "Customer to upgrade:"
-//    let customerBefore = getCustomer id
-//    customerBefore |> service.GetCustomerInfo |> printfn "%s"
-//    printfn ""
-//    printfn "Upgrading customer..."
-//    let customerAfter = service.UpgradeCustomer id
-//    printfn ""
-//    printfn "Customer upgraded:"
-//    customerAfter |> service.GetCustomerInfo |> printfn "%s"
-//    printfn ""
-//    printfn "Press enter to try again"
-    Console.ReadLine () |> ignore
-    main args
+//    if not valid then
+//        printfn "Invalid customer Id"
+//    else
+//        printfn "Customer to upgrade:"
+//        let customerBefore = getCustomer id
+//        customerBefore |> service.GetCustomerInfo |> printfn "%s"
+//        printfn ""
+//        printfn "Upgrading customer..."
+//        let customerAfter = service.UpgradeCustomer id
+//        printfn ""
+//        printfn "Customer upgraded:"
+//        customerAfter |> service.GetCustomerInfo |> printfn "%s"
+    printfn ""
+    printfn "Press any key to try again or 'q' to quit"
+    let input = Console.ReadKey()
+    printfn ""
+    if input.Key = ConsoleKey.Q then 0 else main args
