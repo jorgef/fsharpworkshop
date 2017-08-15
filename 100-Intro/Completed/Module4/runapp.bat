4bdebc44027c75e55d8994457a403bfcb0172090
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2017-08-15 07:20:05 -0400

    Created 100-Intro folder and adjusted packages paths

---
+++ 100-Intro/Completed/Module4/runapp.bat
@@ -0,0 +1,11 @@
+..\..\..\.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+    exit /b %errorlevel%
+)
+
+..\..\..\.paket\paket.exe restore
+if errorlevel 1 (
+    exit /b %errorlevel%
+)
+
+..\..\..\packages\FAKE\tools\FAKE.exe RunApp.fsx %*
---
+++ 100-Intro/Completed/Module4/runapp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mono --runtime=v4.0 ../../../.paket/paket.bootstrapper.exe
+mono --runtime=v4.0 ../../../.paket/paket.exe restore
+mono --runtime=v4.0 ../../../packages/FAKE/tools/FAKE.exe RunApp.fsx $@
