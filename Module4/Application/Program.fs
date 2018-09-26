b4f4f512a8d517a72712f337b70e0d0bf69e57e9
Jorge <jorge.fioranelli@gmail.com>
2018-09-26 01:10:54 -0400

    Updated exercises to support dotnet core

--- Module4/Application/Program.fs
+++ Module4/Application/Program.fs
@@ -6,22 +6,22 @@ open Functions
 
 [<EntryPoint>]
 let rec main args =
-//    let service = CustomerService()
-//    printf "Id to upgrade [1-4]: "
-//    let valid, id = Int32.TryParse <| Console.ReadLine ()
-//    printfn ""
-//    if not valid then
-//        printfn "Invalid customer Id"
-//    else
-//        printfn "Customer to upgrade:"
-//        let customerBefore = getCustomer id
-//        customerBefore |> service.GetCustomerInfo |> printfn "%s"
-//        printfn ""
-//        printfn "Upgrading customer..."
-//        let customerAfter = service.UpgradeCustomer id
-//        printfn ""
-//        printfn "Customer upgraded:"
-//        customerAfter |> service.GetCustomerInfo |> printfn "%s"
+    // let service = CustomerService()
+    // printf "Id to upgrade [1-4]: "
+    // let valid, id = Int32.TryParse <| Console.ReadLine ()
+    // printfn ""
+    // if not valid then
+    //    printfn "Invalid customer Id"
+    // else
+    //    printfn "Customer to upgrade:"
+    //    let customerBefore = getCustomer id
+    //    customerBefore |> service.GetCustomerInfo |> printfn "%s"
+    //    printfn ""
+    //    printfn "Upgrading customer..."
+    //    let customerAfter = service.UpgradeCustomer id
+    //    printfn ""
+    //    printfn "Customer upgraded:"
+    //    customerAfter |> service.GetCustomerInfo |> printfn "%s"
     printfn ""
     printfn "Press any key to try again or 'q' to quit"
     let input = Console.ReadKey ()
